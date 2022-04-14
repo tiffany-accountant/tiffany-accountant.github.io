69919e0504e383428ec6149d1d09b3f4cc909a21
KelvH <kelvin@imagexmedia.com>
2022-04-13 21:44:47 -0700

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+   let typed = new Typed(".typed-element", {
+      strings: ["business.", "retirement savings.", "pocket."],
+      typeSpeed: 70,
+      loop: true,
+      startDelay: 1000,
+      showCursor: false
+   });
+});
